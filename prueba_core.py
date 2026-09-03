305f41d04f57e1e230f31e44eaf7e65b854fd811
Ariel Fernandez <lic.arielfernandez.hys@gmail.com>
2026-09-02 21:07:46 -0300

    Excluye archivo de pruebas local del control de versiones

--- prueba_core.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from src.triton_telemetry.core import scan_all_providers
-
-async def main():
-    resultados = await scan_all_providers(["AWS", "Azure", "GCP"], timeout=1.5, use_chaos=True)
-    for r in resultados:
-        print(r)
-
-asyncio.run(main())
